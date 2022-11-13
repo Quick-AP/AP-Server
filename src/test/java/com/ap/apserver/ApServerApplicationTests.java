c56a9d30bf321dbd40de271e8ca12f7cc7f3ca25
Qiaolin Yu <liin1211@outlook.com>
2022-11-13 11:38:45 +0800

    first commit

---
+++ src/test/java/com/ap/apserver/ApServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ap.apserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
