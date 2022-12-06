9588d8bc20a1e36493e13e9cdbfc39bc34cd4117
Magul-L <doublelight77@gmail.com>
2022-12-06 14:43:46 +0800

    debug getOderById()

--- src/main/java/com/ap/apserver/service/impl/OrderServiceImpl.java
+++ src/main/java/com/ap/apserver/service/impl/OrderServiceImpl.java
@@ -51,6 +51,7 @@ public class OrderServiceImpl implements OrderService {
         order.setSumPrice(sumPrice(tableId));
         order.setFoodIdList(foodIdList);
         return order;
+
     }
 
     @Override
